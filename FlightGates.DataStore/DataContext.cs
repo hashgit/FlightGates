7f7c156ab5f12fcb7587b1eaea9dd8ada279647f
Imad.Hashmi <Imad.Hashmi@gmail.com>
2016-05-05 00:02:49 +1000

    fixed flights and gates count

--- FlightGates.DataStore/DataContext.cs
+++ FlightGates.DataStore/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FlightGates.DataStore.Entities;
 
 namespace FlightGates.DataStore
@@ -20,12 +21,40 @@ namespace FlightGates.DataStore
             Gates = new List<Gate> { new Gate { Id = ++GateCount, Flights = new List<Flight>() }, new Gate { Id = ++GateCount, Flights = new List<Flight>() } };
             Flights = new List<Flight>
                 {
-                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = new DateTime(2016, 05, 01, 02, 30, 00), DepartureDateTime = new DateTime(2016, 05, 01, 03, 00, 00)},
-                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = new DateTime(2016, 05, 01, 03, 00, 00), DepartureDateTime = new DateTime(2016, 05, 01, 03, 30, 00)}
-                };
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(1), DepartureDateTime = DateTime.Today.AddHours(1).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(2), DepartureDateTime = DateTime.Today.AddHours(2).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(3), DepartureDateTime = DateTime.Today.AddHours(3).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(4), DepartureDateTime = DateTime.Today.AddHours(4).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(5), DepartureDateTime = DateTime.Today.AddHours(5).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(6), DepartureDateTime = DateTime.Today.AddHours(6).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(7), DepartureDateTime = DateTime.Today.AddHours(7).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(8), DepartureDateTime = DateTime.Today.AddHours(8).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(9), DepartureDateTime = DateTime.Today.AddHours(9).AddMinutes(30)},
+                    new Flight { Gate = Gates[0], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(10), DepartureDateTime = DateTime.Today.AddHours(10).AddMinutes(30)},
 
-            Gates[0].Flights.Add(Flights[0]);
-            Gates[0].Flights.Add(Flights[1]);
+                    // gate 1
+
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(1), DepartureDateTime = DateTime.Today.AddHours(1).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(2), DepartureDateTime = DateTime.Today.AddHours(2).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(3), DepartureDateTime = DateTime.Today.AddHours(3).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(4), DepartureDateTime = DateTime.Today.AddHours(4).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(5), DepartureDateTime = DateTime.Today.AddHours(5).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(6), DepartureDateTime = DateTime.Today.AddHours(6).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(7), DepartureDateTime = DateTime.Today.AddHours(7).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(8), DepartureDateTime = DateTime.Today.AddHours(8).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(9), DepartureDateTime = DateTime.Today.AddHours(9).AddMinutes(30)},
+                    new Flight { Gate = Gates[1], Id = ++FlightCount, ArrivalDateTime = DateTime.Today.AddHours(10), DepartureDateTime = DateTime.Today.AddHours(10).AddMinutes(30)},
+            };
+
+            foreach (var flight in Flights.Where(f => f.Gate == Gates[0]))
+            {
+                Gates[0].Flights.Add(flight);
+            }
+
+            foreach (var flight in Flights.Where(f => f.Gate == Gates[1]))
+            {
+                Gates[1].Flights.Add(flight);
+            }
         }
 
         public IList<Gate> Gates { get; set; }
